3eb5d3ce2eaab8a1f0c5a2507f648bfe193ceb85
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-22 15:19:05 +0800

    Update start-mac.sh

--- scripts/aelf-node/start-mac.sh
+++ scripts/aelf-node/start-mac.sh
@@ -8,7 +8,3 @@ cp scripts/aelf-node/keys/SD6BXDrKT2syNd1WehtPyRo3dPBiXqfGUj8UJym7YP9W9RynM.json
 cp scripts/aelf-node/app* aelf/
 echo "start node"
 cd aelf && dotnet AElf.Launcher.dll >/dev/null 2>&1 &
-# echo "sleep 10"
-# sleep 10
-# echo "curl"
-# curl  -s http://10.79.1.150:8000/api/blockChain/blockHeight
